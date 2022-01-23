488da983fa9bfa7aaeac17e801dde594fcf768f0
yashshah7197 <yash@yashshah.io>
2022-01-23 10:06:58 -0500

    Add support for generating JSON Web Tokens

--- app/tooling/admin/main.go
+++ app/tooling/admin/main.go
@@ -6,12 +6,25 @@ import (
 	"crypto/x509"
 	"encoding/pem"
 	"fmt"
+	"io"
 	"os"
+	"time"
+
+	"github.com/lestrrat-go/jwx/jwa"
+	"github.com/lestrrat-go/jwx/jws"
+	"github.com/lestrrat-go/jwx/jwt"
 )
 
 func main() {
+	// Generate a new private/public key pair.
 	err := genKeyPair()
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(1)
+	}
 
+	// Generate a new, signed JSON Web Token.
+	err = genToken()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
@@ -67,6 +80,60 @@ func genKeyPair() error {
 	if err := pem.Encode(publicKeyFile, &publicBlock); err != nil {
 		return fmt.Errorf("encoding to public key file: %w", err)
 	}
+
+	return nil
+}
+
+// genToken generates a new, signed JSON Web Token.
+func genToken() error {
+	// Open the private key file for reading.
+	privateFile, err := os.Open("private.pem")
+	if err != nil {
+		return fmt.Errorf("opening private key file: %w", err)
+	}
+
+	// Read the contents of the private key file.
+	privateBytes, err := io.ReadAll(privateFile)
+	if err != nil {
+		return fmt.Errorf("reading private key file: %w", err)
+	}
+
+	// Decode the contents of the private key file in to a PEM block.
+	privatePEM, _ := pem.Decode(privateBytes)
+
+	// Parse the private key PEM block in to a private key.
+	privateKey, err := x509.ParsePKCS1PrivateKey(privatePEM.Bytes)
+	if err != nil {
+		return fmt.Errorf("parsing private key pem block: %w", err)
+	}
+
+	// Generate a new JSON Web Token.
+	token, err := jwt.NewBuilder().
+		Issuer("shrt-api").
+		Subject("b0ef2788-614a-47b6-a7ba-c0c7c75f6d7f").
+		IssuedAt(time.Now().UTC()).
+		Expiration(time.Now().Add(8760*time.Hour).UTC()).
+		Claim("roles", []string{"admin"}).
+		Build()
+	if err != nil {
+		return fmt.Errorf("generating jwt: %w", err)
+	}
+
+	// Add the key ID header.
+	headers := jws.NewHeaders()
+	err = headers.Set("kid", "905789cb-61d7-44c8-a7e2-0e51d43d8c85")
+	if err != nil {
+		return fmt.Errorf("setting jwt headers: %w", err)
+	}
+
+	// Sign the token with our private key.
+	signedToken, err := jwt.Sign(token, jwa.RS256, privateKey, jwt.WithHeaders(headers))
+	if err != nil {
+		return fmt.Errorf("signing token with the private key: %w", err)
+	}
+
+	// Print the signed token to STDOUT.
+	fmt.Println(string(signedToken))
 
 	return nil
 }
--- go.mod
+++ go.mod
@@ -6,11 +6,21 @@ require (
 	github.com/ardanlabs/conf v1.5.0
 	github.com/go-chi/chi/v5 v5.0.7
 	github.com/google/uuid v1.3.0
+	github.com/lestrrat-go/jwx v1.2.18
 	go.uber.org/automaxprocs v1.4.0
 	go.uber.org/zap v1.19.1
 )
 
 require (
+	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.0-20210816181553-5444fa50b93d // indirect
+	github.com/goccy/go-json v0.9.4 // indirect
+	github.com/lestrrat-go/backoff/v2 v2.0.8 // indirect
+	github.com/lestrrat-go/blackmagic v1.0.0 // indirect
+	github.com/lestrrat-go/httpcc v1.0.0 // indirect
+	github.com/lestrrat-go/iter v1.0.1 // indirect
+	github.com/lestrrat-go/option v1.0.0 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
+	golang.org/x/crypto v0.0.0-20201217014255-9d1352758620 // indirect
 )
